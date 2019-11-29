705b13e3ae53446a7e11ddfbcc13178f99718606
MattRamosManzano <58317114+MattRamosManzano@users.noreply.github.com>
2019-11-28 21:35:50 -0500

    Add files via upload
    Working version.

---
+++ ToogoodChallenge/ToogoodChallenge/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ToogoodChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // I assumed one record per row instead of one value per row (one record in many adjacent rows).
+            string valuePerRow = "Identifier\nName\nType\nOpened\nCurrency\n123|AbcCode\nMy Account\n2\n01-01-2018\nCD";
+            string recordPerRow1 = "Identifier,Name,Type,Opened,Currency\n123|AbcCode,My Account,2,01-01-2018,CD";
+            string recordPerRow2 = "My Account,2,C,";
+
+            Dictionary<string, (string, Func<string, string>)> maps = new Dictionary<string, (string, Func<string, string>)>();
+
+            maps.Add("AccountCode", ("Identifier",
+                x =>
+                {
+                    return x.Split("|")[1];
+                }
+            ));
+
+            static string typeConvert(string x)
+            {
+                if (x == "1")
+                {
+                    return "Trading";
+                }
+                else if (x == "2")
+                {
+                    return "RRSP";
+                }
+                else if (x == "3")
+                {
+                    return "RESP";
+                }
+                else if (x == "4")
+                {
+                    return "Fund";
+                }
+                else
+                {
+                    return "Error";
+                }
+
+            }
+
+            static string dateConvert(string x)
+            {
+                return DateTime.ParseExact(x, "dd-mm-yyyy", CultureInfo.InvariantCulture).ToString("yyyy-MM-dd");
+            }
+
+            maps.Add("Name", ("Name", x => x));
+            maps.Add("Type", ("Type", (Func<string, string>)typeConvert));
+            maps.Add("Open Date", ("Opened", (Func<string, string>)dateConvert));
+
+
+
+            maps.Add("Currency", ("Currency", x =>
+            {
+                if (x == "CD")
+                {
+                    return "CAD";
+                }
+                else if (x == "US")
+                {
+                    return "USD";
+                }
+                else
+                {
+                    return "Error";
+                }
+            }
+            ));
+
+            Dictionary<string, (int, Func<string, string>)> format1Conversions = new Dictionary<string, (int, Func<string, string>)>();
+
+            format1Conversions.Add("AccountCode", (0,
+                x =>
+                {
+                    return x.Split("|")[1];
+                }
+            ));
+
+
+            format1Conversions.Add("Name", (1, x => x));
+            format1Conversions.Add("Type", (2, (Func<string, string>)typeConvert));
+            format1Conversions.Add("Open Date", (3, (Func<string, string>)dateConvert));
+
+
+
+            format1Conversions.Add("Currency", (4, x =>
+            {
+                if (x == "CD")
+                {
+                    return "CAD";
+                }
+                else if (x == "US")
+                {
+                    return "USD";
+                }
+                else
+                {
+                    return "Error";
+                }
+            }
+            ));
+
+
+
+            Console.WriteLine("Format given in email:");
+            Console.WriteLine(TransformSystem.TransformAnyFileFormat(valuePerRow, 5, maps));
+
+
+            // Format 1
+            Console.WriteLine("--------------------------------\nCSV format 1:");
+            Console.WriteLine(TransformSystem.TransformAnyCSVFormat(recordPerRow1, true, format1Conversions));
+
+            Dictionary<string, (int, Func<string, string>)> format2Conversions = new Dictionary<string, (int, Func<string, string>)>();
+            format2Conversions.Add("AccountCode", (3, x => x));
+            format2Conversions.Add("Name", (0, x => x));
+            format2Conversions.Add("Type", (1, (Func<string, string>)typeConvert));
+            format2Conversions.Add("Open Date", (0, x => ""));
+            format2Conversions.Add("Currency", (2, x =>
+            {
+                if (x == "C")
+                {
+                    return "CAD";
+                } else if (x == "U")
+                {
+                    return "USD";
+                }
+                else
+                {
+                    return "Err";
+                }
+            }
+            ));
+
+            // Format 2
+            Console.WriteLine("--------------------------------\nCSV format 2:");
+            Console.WriteLine(TransformSystem.TransformAnyCSVFormat(recordPerRow2, false, format2Conversions));
+
+
+            string example1 = "Title,Type,Opened Date,Name,Currency\nAbcCode,RRSP,2018-01-01,MyAccount,Canadian";
+            Dictionary<string, (int, Func<string, string>)> example1Conversions = new Dictionary<string, (int, Func<string, string>)>();
+            example1Conversions.Add("AccountCode", (0, x => x));
+            example1Conversions.Add("Name", (3, x => x));
+            example1Conversions.Add("Type", (1, x => x));
+            example1Conversions.Add("Open Date", (2, x => x));
+            example1Conversions.Add("Currency", (4, x =>
+            {
+                if (x == "Canadian")
+                {
+                    return "CAD";
+                } else
+                {
+                    return "USD";
+                }
+            }
+            ));
+
+            // example 1
+            Console.WriteLine("--------------------------------\nCSV format example 1:");
+            Console.WriteLine(example1);
+            Console.WriteLine("Output:");
+            Console.WriteLine(TransformSystem.TransformAnyCSVFormat(example1, true, example1Conversions));
+
+
+            // example 2
+            string example2 = "Abc|1,2019-01-01,RRSP,CAD,Account 1\nAbc|2,,RESP,USD,Account 2\nDef|1,2019-01-02,Trading,USD,Account 3";
+            Dictionary<string, (int, Func<string, string>)> example2Conversions = new Dictionary<string, (int, Func<string, string>)>();
+            example2Conversions.Add("AccountCode", (0, x => x.Replace("|", "-")));
+            example2Conversions.Add("Name", (4, x => x));
+            example2Conversions.Add("Type", (2, x => x));
+            example2Conversions.Add("Open Date", (1, x => x));
+            example2Conversions.Add("Currency", (3, x => x));
+
+
+            Console.WriteLine("--------------------------------\nCSV format example 2:");
+            Console.WriteLine(example2);
+            Console.WriteLine("Output:");
+            Console.WriteLine(TransformSystem.TransformAnyCSVFormat(example2, false, example2Conversions));
+        }
+
+    }
+
+    
+}
---
+++ ToogoodChallenge/ToogoodChallenge/TransformSystem.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace ToogoodChallenge
+{
+    sealed class TransformSystem
+    {
+        private static TransformSystem instance;
+        private const int TargetHeaderLength = 5;   // length of the header (number of columns ... or rows for email format)
+        private const int FileFormatTwoPseudoHeaderRows = 4;    // number of header rows in email format 2
+
+        private TransformSystem() { }
+
+        public static TransformSystem Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new TransformSystem();
+                }
+
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Converts any type of file to the target desired, if given an ordered dictionary of conversions from source to target and the number of headers in the source.
+        /// Assumptions: 
+        /// 1. one VALUE per line
+        /// 2. Missing values without a record are blank -- empty line (i.e. All records take up the same amount of lines.)
+        /// 3. Every file except format 2 will have headers.
+        /// 4. Newlines are not in unexpected places.
+        /// </summary>
+        /// <param name="file">The input file or source file as a string where each row is separated by a newline.</param>
+        /// <param name="headerRows">The number of header rows present in the source.</param>
+        /// <param name="conversions">A dictionary of mappings that convert a source row to a target row.
+        /// The order for the dictionary is based on the target ordering. For this case that means the keys are ordered as such:
+        /// AccountCode, Name, Type, Open Date, Currency.
+        /// </param>
+        /// <returns>The output as one string with each row separated by new lines.</returns>
+        public static string TransformAnyFileFormat(string file, int headerRows, Dictionary<string, (string, Func<string, string>)> conversions)
+        {
+            
+
+            // output
+            string finalConversion = "";
+
+            // add header for target
+            foreach (string key in conversions.Keys)
+            {
+                finalConversion += key + "\n";
+            }
+
+            // split the file by newlines
+            string[] fileContents =
+                file.Split(
+                    new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None
+                );
+
+            // header rows of source
+            string[] headers = fileContents.Take(headerRows).ToArray();
+
+            // record rows of source
+            string[] records = fileContents.Skip(headerRows).ToArray();
+
+            // number of records based on header rows; assume missing values are a blank line
+            int recordCount = headerRows != 0 ? records.Length / TargetHeaderLength : (records.Length / headerRows) - 1;
+
+            // length of record based on header rows
+            int recordRowLength = headerRows == 0 ? FileFormatTwoPseudoHeaderRows : headerRows;
+
+            // for each record
+            for (int index = 0; index < recordCount; index++)
+            {
+                // the current record based on record length
+                string[] currentRecord = records.Skip(index * recordRowLength).Take(recordRowLength).ToArray();
+
+                // for each mapping
+                foreach (KeyValuePair<string, (string, Func<string, string>)> c in conversions)
+                {
+                    // find the row in the current source record that converts to current target row mapping
+                    int relevantRow = Array.IndexOf(headers, c.Value.Item1);
+                    Func<string, string> fn = c.Value.Item2;
+
+                    // append the record value to the return value
+                    finalConversion += fn(currentRecord[relevantRow]) + "\n";
+                }
+
+            }
+
+            return finalConversion;
+        }
+
+
+
+        /// <summary>
+        /// Converts any type of csv file to the target desired, if given an ordered dictionary of conversions from source to target and whether there are headers in the source.
+        /// Assumptions: 
+        /// 1. one ROW per line
+        /// 2. Missing values without a record are blank.
+        /// 3. Commas do not exist within a column.
+        /// </summary>
+        /// <param name="file">The input file or source file as a string where each column within a row is split by commas and each row is separated by a newline.</param>
+        /// <param name="hasHeaders">If the source has a header row</param>
+        /// <param name="conversions">A dictionary of mappings that convert a source column (by zero based index) to a target column.
+        /// The order for the dictionary is based on the target ordering. For this case that means the keys are ordered as such:
+        /// AccountCode, Name, Type, Open Date, Currency.
+        /// </param>
+        /// <returns>The output as one string with each row separated by new lines in the target format.</returns>
+        public static string TransformAnyCSVFormat(string file, bool hasHeader, Dictionary<string, (int, Func<string, string>)> conversions)
+        {
+
+            // output
+            string finalConversion = "";
+
+            // add header for target
+            finalConversion += String.Join(",", conversions.Keys) + "\n";
+           
+
+            // split the file by newlines
+            string[] fileContents =
+                file.Split(
+                    new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None
+                );
+
+            // record rows of source
+            string[] records = fileContents.Skip(hasHeader ? 1 : 0).ToArray();
+
+            // for each record
+            foreach (string record in records)
+            {
+                // split record by commas -- assumes no commas within a column
+                string[] recordColumns = record.Split(",").ToArray();
+                string[] currentRecordConversion = new string[TargetHeaderLength];
+
+                int recordIndex = 0;
+                // for each mapping
+                foreach (KeyValuePair<string, (int, Func<string, string>)> c in conversions)
+                {
+                    // find the column in the current source record that converts to current target row mapping
+                    int relevantColumn = c.Value.Item1;
+                    Func<string, string> fn = c.Value.Item2;
+
+                    // append the record value to the return value
+                    currentRecordConversion[recordIndex] = fn(recordColumns[relevantColumn]);
+                    recordIndex++;
+                }
+
+                finalConversion += String.Join(",", currentRecordConversion) + "\n";
+            }
+
+            return finalConversion;
+        }
+
+
+        /// <summary>
+        /// Ignore. Made for trying format 1 before improvements.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static string TransformFileFormat1(string file)
+        {
+            /* Assumptions: Missing values without a record are blank -- empty line (i.e. will always be 5 rows)
+             * 
+             */
+
+            string finalConversion = "";
+            int headerRows = 5;
+
+            Dictionary<string, (string, Func<string, string>)> conversions = new Dictionary<string, (string, Func<string, string>)>();
+
+            conversions.Add("AccountCode", ("Identifier",
+                x =>
+                {
+                    return x.Split("|")[1];
+                }
+            ));
+
+            Func<string, string> typeConvert = x =>
+            {
+                if (x == "1")
+                {
+                    return "Trading";
+                }
+                else if (x == "2")
+                {
+                    return "RRSP";
+                }
+                else if (x == "3")
+                {
+                    return "RESP";
+                }
+                else if (x == "4")
+                {
+                    return "Fund";
+                }
+                else
+                {
+                    return "Error";
+                }
+
+            };
+
+            Func<string, string> dateConvert = x =>
+            {
+                return DateTime.ParseExact(x, "dd-mm-yyyy", CultureInfo.InvariantCulture).ToString("yyyy-MM-dd");
+            };
+
+            conversions.Add("Name", ("Name", x => x));
+            conversions.Add("Type", ("Type", typeConvert));
+            conversions.Add("Open Date", ("Opened", dateConvert));
+                
+            
+            
+            conversions.Add("Currency", ("Currency", x =>
+            {
+                if (x == "CD")
+                {
+                    return "CAD";
+                } else if (x == "US")
+                {
+                    return "USD";
+                } else
+                {
+                    return "Error";
+                }
+            }));
+
+
+                
+            foreach (string key in conversions.Keys)
+            {
+                finalConversion += key + "\n";
+            }           
+                        
+
+
+            string[] fileContents =
+                file.Split(
+                    new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None
+                );
+
+            string[] headers = fileContents.Take(headerRows).ToArray();
+            string[] records = fileContents.Skip(headerRows).ToArray();
+
+            int recordCount = headerRows != 0 ?  records.Length / TargetHeaderLength : (records.Length / headerRows) - 1;
+
+            for (int index = 0; index < recordCount; index++)
+            {
+                string[] currentRecord = records.Skip(index * headerRows).Take(headerRows).ToArray();
+                
+                string[] convertedRecord = new string[TargetHeaderLength];
+                int recordRowCounter = 0;
+
+                foreach (KeyValuePair<string, (string, Func<string, string>)> c in conversions)
+                {
+                    int relevantRow = Array.IndexOf(headers, c.Value.Item1);
+                    Func<string, string> fn = c.Value.Item2;
+
+                    convertedRecord[recordRowCounter] = fn(currentRecord[relevantRow]);
+                    finalConversion += fn(currentRecord[relevantRow]) + "\n";
+                }
+                
+
+                
+
+            }
+            
+            return finalConversion;
+        }
+    }
+}
